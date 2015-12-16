3f29470257e243afd45a91d6af923da9788963f4
Jon Hutchison <jonmhutch@mac.com>
2015-12-15 21:45:05 -0600

    Removed unnecessary header bar. Added app colors. changed styles.

--- src/components/Application/Application.js
+++ src/components/Application/Application.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {root} from 'baobab-react/higher-order';
 import tree from 'state/StateTree';
-import Header from './components/Header';
 import SideBar from './components/SideBar';
 import Footer from './components/Footer';
 import _ from 'lodash';
@@ -17,7 +16,6 @@ class App extends React.Component {
     }, '');
     return (
       <div className={classNames.trim()+'-wrapper ' + 'main-container'}>
-        <Header />
         {
           classNames.trim() !== 'login' ? 
           <SideBar /> : null
