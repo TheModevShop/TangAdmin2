701b763a5985ba864c759393126b034e0a8e3223
Vincent W Profeta <vprofeta12@gmail.com>
2016-01-05 19:16:11 -0600

    added user actions back in

---
+++ src/actions/UserActions.js
@@ -0,0 +1,54 @@
+import tree from 'state/StateTree';
+import {editMeApi, getUser} from 'api/userApi';
+import _ from 'lodash';
+import history from 'appHistory';
+import {teardownSession} from 'actions/authenticationActions';
+import {getMyGym} from 'actions/GymActions';
+import {getRoles} from 'actions/RolesActions';
+
+const userCursor = tree.select(['user']);
+const userDetailsCurosr = tree.select(['user', 'details']);
+
+userDetailsCurosr.on('update', async (value) => {
+  const location = _.get(value, 'data.data.gyms[0]');
+  const locationId = _.get(location, 'gym');
+  if (locationId) {
+    await getMyGym(locationId);
+    userCursor.set('role', location.role.name);
+    tree.commit();
+  }
+});
+
+export async function editMe(data) {
+  try {
+    await editMeApi(data);
+    await getMe();
+  } catch (err) {
+    return err
+  }
+}
+
+export async function getMe() {
+  const user = await getUser();
+  if (user.body._id) {
+    userCursor.set(['details'], user.body);
+    tree.commit();
+    return user.body;
+  } else {
+    userCursor.set(['details'], null);
+    teardownSession();
+    history.pushState(null, '/login');
+  }
+  return user;
+}
+
+export async function setDefaultLocation(data) {
+  const defaultLocationArray = defaultLocation.get(data) || [];
+  defaultLocationArray.push(data)
+  defaultLocation.set(defaultLocationArray);
+  tree.commit();
+  await editMe({gyms: [defaultLocationArray[0]._id]});
+  invalidateAfterGymChange();
+}
+
+
