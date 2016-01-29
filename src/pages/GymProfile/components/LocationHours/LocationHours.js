86651c7c7b09dbdf8199be9c26bc71137e26541b
Vincent W Profeta <vprofeta12@gmail.com>
2016-01-28 21:10:14 -0600

    fixed hours

--- src/pages/GymProfile/components/LocationHours/LocationHours.js
+++ src/pages/GymProfile/components/LocationHours/LocationHours.js
@@ -15,49 +15,49 @@ class LocationHours extends React.Component {
               <span className="day-label">Monday:</span>
               { profile.hours.mon_open ?
                 <div>{profile.hours.mon_open} - {profile.hours.mon_close}</div>
-                : Closed
+                : 'Closed'
               }
             </div>
             <div>
               <span className="day-label">Tuesday:</span>
               { profile.hours.tue_open ?
                 <div>{profile.hours.tue_open} - {profile.hours.tue_close}</div>
-                : Closed
+                : 'Closed'
               }
             </div>
             <div>
               <span className="day-label">Wednesday:</span>
               { profile.hours.wed_open ?
                 <div>{profile.hours.wed_open} - {profile.hours.wed_close}</div>
-                : Closed
+                : 'Closed'
               }
             </div>
             <div>
               <span className="day-label">Thursday:</span>
               { profile.hours.thu_open ?
                 <div>{profile.hours.thu_open} - {profile.hours.thu_close}</div>
-                : Closed
+                : 'Closed'
               }
             </div>
             <div>
               <span className="day-label">Friday:</span>
               { profile.hours.fri_open ?
                 <div>{profile.hours.fri_open} - {profile.hours.fri_close}</div>
-                : Closed
+                : 'Closed'
               }
             </div>
             <div>
               <span className="day-label">Saturday:</span>
               { profile.hours.sat_open ?
                 <div>{profile.hours.sat_open} - {profile.hours.sat_close}</div>
-                : Closed
+                : 'Closed'
               }
             </div>
             <div>
               <span className="day-label">Sunday:</span>
               { profile.hours.sun_open ?
                 <div>{profile.hours.sun_open} - {profile.hours.sun_open}</div>
-                : Closed
+                : 'Closed'
               }
             </div>
           </div>
