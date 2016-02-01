be9168ae02dfd5baf7ca4433681adf5c92b3e063
Jon Hutchison <jonmhutch@mac.com>
2016-02-01 16:25:35 -0600

    Added image uploader.

--- src/pages/AddGym/components/photos.jsx
+++ src/pages/AddGym/components/photos.jsx
@@ -1,24 +1,70 @@
 import React from 'react';
-import Image from 'react';
-import {Row, Col} from 'react-bootstrap';
-import FileInput from 'react-file-input';
+import {Row, Col, Button} from 'react-bootstrap';
+var Dropzone = require('react-dropzone');
 import './../add-gym.less';
 
-var PhotosComponent = React.createClass({
-    handleChange(event) {
-      console.log('Selected file:', event.target.files[0]);
+const PhotosComponent = React.createClass({
+    getInitialState() {
+        return {
+          images: []
+        };
     },
-	render() {
-		return (
+
+    onDrop(files) {
+        const imgs = this.state.images;
+
+        for (var file in files) {
+            imgs.push(files[file]);
+        }
+
+        this.setState({
+            images: imgs
+        });
+    },
+
+    onSubmit() {
+      console.log(this.state.images)
+    },
+
+    render() {
+        return (
             <Row>
                 <Col xs={12}>
-        			<Row>
-                        <FileInput name="myImage" accept=".png,.gif" placeholder="My Image" className="inputClass" onChange={this.handleChange} />
+                    <Row>
+                        <Col xs={12}>
+                            <Row>
+                                {
+                                    this.state.images.length > 0 ? 
+                                            this.state.images.map((file) =>
+                                                <Col xs={4} className="image-container">
+                                                    <div>
+                                                        <img key={file.lastModified} src={file.preview} /> 
+                                                    </div>
+                                                </Col>
+                                            )
+                                : 
+                                    null
+                                }
+                                <Col className="dropzone-container" xs={4}>
+                                    <Dropzone className="dropzone" ref="dropzone" onDrop={this.onDrop}>
+                                        <span>+</span>
+                                        <p>Upload Photos</p>
+                                    </Dropzone>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12}>
+                            <Button type="button" onClick={this.onSubmit}>
+                               Update
+                            </Button>
+                        </Col>
                     </Row>
                 </Col>
             </Row>
-		);
-	}
+        );
+    }
 });
 
 export default PhotosComponent;
