22b54b75c9fe9c2a35ade637b8ca9698725090df
Vincent.Profeta <Vincent@impossibleventures.com>
2016-02-22 18:00:00 -0600

    clean up add photos

--- src/pages/AddGym/components/photos.jsx
+++ src/pages/AddGym/components/photos.jsx
@@ -6,73 +6,71 @@ import {addPhotos} from 'actions/AddGymActions';
 import './../add-gym.less';
 
 const PhotosComponent = React.createClass({
-    getInitialState() {
-      return {
-        images: []
-      };
-    },
+  getInitialState() {
+    return {
+      images: []
+    };
+  },
 
-    onDrop(files) {
-      const imgs = this.state.images;
-      for (var file in files) {
-          imgs.push(files[file]);
-      }
-
-      this.setState({
-          images: imgs
-      });
-    },
-
-    onDelete(files) {
-        
-    },
-
-    onSubmit() {
-      const images = _.map(this.state.images, (image) => {
-        return image.preview;
-      })
-      addPhotos(images, this.props.gymId);
-    },
-
-    render() {
-        return (
-            <Row>
-                <Col xs={12}>
-                    <Row>
-                        <Col xs={12}>
-                            <Row>
-                                {
-                                    this.state.images.length > 0 ? 
-                                            this.state.images.map((file) =>
-                                                <Col xs={4} className="image-container">
-                                                    <div>
-                                                        <img key={file.lastModified} src={file.preview} /> 
-                                                    </div>
-                                                </Col>
-                                            )
-                                : 
-                                    null
-                                }
-                                <Col className="dropzone-container" xs={4}>
-                                    <Dropzone className="dropzone" ref="dropzone" onDrop={this.onDrop}>
-                                        <span>+</span>
-                                        <p>Upload Photos</p>
-                                    </Dropzone>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={12}>
-                            <Button type="button" onClick={this.onSubmit}>
-                               Update
-                            </Button>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        );
+  onDrop(files) {
+    const imgs = this.state.images;
+    for (var file in files) {
+      imgs.push(files[file]);
     }
+
+    this.setState({
+      images: imgs
+    });
+  },
+
+  onDelete(files) {
+
+  },
+
+  onSubmit() {
+    const images = _.map(this.state.images, (image) => {
+      return image.preview;
+    })
+    addPhotos(images, this.props.gymId);
+  },
+
+  render() {
+    return (
+      <Row>
+        <Col xs={12}>
+          <Row>
+            <Col xs={12}>
+              <Row>
+                {
+                  this.state.images.length > 0 ? 
+                  this.state.images.map((file) =>
+                    <Col xs={4} className="image-container">
+                      <div>
+                      <img key={file.lastModified} src={file.preview} /> 
+                    </div>
+                  </Col>
+                  ) : null
+                }
+                <Col className="dropzone-container" xs={4}>
+                  <Dropzone className="dropzone" ref="dropzone" onDrop={this.onDrop}>
+                    <span>+</span>
+                    <p>Upload Photos</p>
+                  </Dropzone>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <Button type="button" onClick={this.onSubmit}>
+                Update
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    );
+  }
 });
 
 export default PhotosComponent;
